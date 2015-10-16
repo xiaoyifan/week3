5f19daae613639eb3a9e0d80842db10b398e2073
Jeff Cohen <cohen.jeff@gmail.com>
2015-10-15 19:47:31 -0500

    Use remote: true

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,9 @@ class ItemsController < ApplicationController
     item = list.items.create(content: params[:content])
 
     respond_to do |format|
+      format.js do
+        render js: "$('ul').append('<li>#{item.content}</li>'); $('#content').val('')"
+      end
       format.html do
         redirect_to list_url(list.id)
       end
