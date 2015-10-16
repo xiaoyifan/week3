c32c8ffdb104f7c5bc1e51e8da0959465e4d3462
Jeff Cohen <cohen.jeff@gmail.com>
2015-10-15 20:14:59 -0500

    Use a partial to DRY up our code

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,17 +2,15 @@ class ItemsController < ApplicationController
 
   def create
     list = List.find(params[:list_id])
-    item = list.items.create(content: params[:content])
+    @item = list.items.create(content: params[:content])
 
     respond_to do |format|
-      format.js do
-        render js: "$('ul').append('<li>#{item.content}</li>'); $('#content').val('')"
-      end
+      format.js
       format.html do
         redirect_to list_url(list.id)
       end
       format.json do
-        render json: item
+        render json: @item
       end
     end
 
